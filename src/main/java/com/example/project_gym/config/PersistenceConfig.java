5ff7de24d80f1a1c3bbaacf6229a1c0c133b8393
IvanYatsevich <ivan.yatsevich13@gmail.com>
2026-06-22 16:20:33 +0200

    Unused classes and imports were deleted after merge

--- src/main/java/com/example/project_gym/config/PersistenceConfig.java
+++ src/main/java/com/example/project_gym/config/PersistenceConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
